1f7057ba97ed5f25842e1c73a0699efdc5337913
ds199895 <2410831777@qq.com>
2023-08-31 14:34:12 +0800

    hsychangetest

--- docs/conf.py
+++ docs/conf.py
@@ -49,6 +49,8 @@ extensions = [
     'sphinx_reredirects',
     'sphinx_toolbox.collapse',
     'mujoco_include',
+    'recommonmark',
+    'sphinx_markdown_tables'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
