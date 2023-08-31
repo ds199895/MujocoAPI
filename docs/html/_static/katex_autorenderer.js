c8e838c77c577db459f157ae4fc17211933f967b
ds199895 <2410831777@qq.com>
2023-08-31 14:16:34 +0800

    hsy0831

---
+++ docs/html/_static/katex_autorenderer.js
@@ -0,0 +1,10 @@
+katex_options = {
+macros: {"\\d":             "\\operatorname{#1}",},
+delimiters: [
+        { left: "\\(", right: "\\)", display: false },
+        { left: "\\[", right: "\\]", display: true }
+        ]
+}
+document.addEventListener("DOMContentLoaded", function() {
+  renderMathInElement(document.body, katex_options);
+});
